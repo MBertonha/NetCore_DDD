ff5da0e6ed81f7a9f900e20c43f763c092a7ec15
Matheus Bertonha <bertonha_2011@hotmail.com>
2020-03-02 15:18:43 -0300

    Criação das Model's

---
+++ src/Api.Domain/Dtos/User/UserDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Dtos.User
+{
+    public class UserDto
+    {
+        [Required(ErrorMessage = "Nome é um campo obrigatório")]
+        [StringLength(60, ErrorMessage = "o nome deve ter no máximo {1} caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email é um campo obrigatório")]
+        [EmailAddress(ErrorMessage = "E-mail no formato invalido")]
+        [StringLength(100, ErrorMessage = "o nome deve ter no máximo {1} caracteres")]
+        public string Email { get; set; }
+    }
+}
